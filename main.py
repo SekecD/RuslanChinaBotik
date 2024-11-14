4bc507b3c98261929316ec1630fe4b5f3dbc1560
SekecD <leshanow2004@mail.ru>
2024-11-14 17:51:06 +0300

    fix

--- main.py
+++ main.py
@@ -7,7 +7,12 @@ bot = telebot.TeleBot(Config.TEST_TOKEN)
 
 user_message_count = {}
 sticker_stats = {}
+user_traits = {}
 
+initial_traits = {
+    "сила": 0, "устойчивость": 0, "броня": 0, "ловкость": 0, "счастье": 0, "спокойствие": 0,
+    "скрытность": 0, "аура": 0, "спиритизм": 0, "фимоз": 0, "уверенность": 0, "криворукость": 0, "хайп": 0
+}
 
 @bot.message_handler(commands=['stats'])
 def send_stats(message):
@@ -18,21 +23,34 @@ def send_stats(message):
         bot.send_message(chat_id, "Пока нет статистики... Немножечко лень")
         return
 
-    sorted_stats = sorted(sticker_stats[chat_id].items(), key=lambda x: x[1]['count'], reverse=True)
-
-    stats_message = "🏆 **Топ получивших стикеры** 🏆\n\n"
+    sorted_stats = sorted(sticker_stats[chat_id].items(), key=lambda x: sum(user_traits[x[0]].values()), reverse=True)
+    stats_message = "🏆 **Топ пользователей по характеристикам** 🏆\n\n"
     stats_message += "Tоп-лист:\n\n"
-
     rank = 1
     for user_id, user_data in sorted_stats:
-        username = user_data.get('name', 'Неизвестный пользователь')
-        sticker_count = user_data.get('count', 0)
-        stats_message += f"{rank}. **{username}**: {sticker_count} стикеров\n"
+        username = f"<a href='tg://user?id={user_id}'>{user_data.get('name', 'Неизвестный')}</a>"
+        total_traits = sum(user_traits[user_id].values())
+        stats_message += f"{rank}. {username}: {total_traits:.0f} очков\n"
         rank += 1
 
     stats_message += "\n🎉 **SEO-Руслан гордится вами!** 🎉"
+    bot.send_message(chat_id, stats_message, parse_mode='HTML')
 
-    bot.send_message(chat_id, stats_message, parse_mode='Markdown')
+@bot.message_handler(commands=['trait'])
+def send_traits(message):
+    chat_id = message.chat.id
+    user_id = message.from_user.id
+    user_name = message.from_user.first_name or "Пользователь"
+
+    if user_id not in user_traits:
+        user_traits[user_id] = initial_traits.copy()
+
+    traits_message = f"📜**Характеристики {user_name}**📜\n\n"
+    for trait, value in user_traits[user_id].items():
+        traits_message += f"**{trait.capitalize()}**: {value}\n"
+
+    bot.send_message(chat_id, traits_message, parse_mode='Markdown')
+
 
 def choose_sticker():
     random_value = random.random()
@@ -43,17 +61,14 @@ def choose_sticker():
             return sticker
     return sticker_pool[-1]
 
-
 @bot.message_handler(func=lambda message: message.chat.type in ['group', 'supergroup'], content_types=['text', 'photo', 'sticker', 'video', 'video_note'])
 def track_user_messages(message):
-
     chat_id = message.chat.id
     user_id = message.from_user.id
     user_name = message.from_user.first_name or "Пользователь"
 
     if chat_id not in user_message_count:
         user_message_count[chat_id] = {}
-
     if user_id not in user_message_count[chat_id]:
         user_message_count[chat_id][user_id] = 0
 
@@ -67,11 +82,16 @@ def track_user_messages(message):
 
         if chat_id not in sticker_stats:
             sticker_stats[chat_id] = {}
-
         if user_id not in sticker_stats[chat_id]:
             sticker_stats[chat_id][user_id] = {'name': user_name, 'count': 0}
 
         sticker_stats[chat_id][user_id]['count'] += 1
+
+        if user_id not in user_traits:
+            user_traits[user_id] = initial_traits.copy()
+
+        for trait, value in chosen_sticker['traits'].items():
+            user_traits[user_id][trait] += value
 
         bot.send_message(
             chat_id,
--- sticker_config.py
+++ sticker_config.py
@@ -1,19 +1,104 @@
 sticker_pool = [
-    {'id': 'CAACAgIAAxkBAAENHXtnNGgqh3m9skg-FG6YcPXGdvHHDAACyVcAAlDTIEkyq5UzKXmolDYE', 'chance': 0.30, 'message': 'Вам выпал монсруозный Ярик'},
-    {'id': 'CAACAgIAAxkBAAENHihnNPqdbLnF0zh0LU5NVooyQ9su9wACvFYAAvS8KEmi8QT8WAQcAAE2BA', 'chance': 0.35, 'message': 'Вам выпал ужаленеый Тигран'},
-    {'id': 'CAACAgIAAxkBAAENHipnNPquoj1u2zoYgZ4E43IqTY9kowACbWkAAkw1KUmjU1q0J3wLmDYE', 'chance': 0.05, 'message': 'Вам выпал бешеный Елисей'},
-    {'id': 'CAACAgIAAxkBAAENHi9nNPq86ruNvyBHZhnESc7DBXGI6wAC3lkAAgL1IUkUjqkTjshZtDYE', 'chance': 0.35, 'message': 'Вам выпал репер Влад'},
-    {'id': 'CAACAgIAAxkBAAENHjFnNPr63UkpFsJk4ffA6VsozGj4vAACEVIAAqGBKUkzzUP2Z6TtnzYE', 'chance': 0.03, 'message': 'Вам выпал гига Руслан'},
-    {'id': 'CAACAgIAAxkBAAENHjNnNPsFZJV8BLLgy2QcO507mceK6QACZFoAAjUoIUn5izprfvq-PzYE', 'chance': 0.10, 'message': 'Вам выпал округленый Руслан'},
-    {'id': 'CAACAgIAAxkBAAENHjVnNPsUgpSdUu6LxkVuwQIO-BHGegACoVcAApbiKUmvO0oITRkKwzYE', 'chance': 0.25, 'message': 'Вам выпал счастливый Кирюша'},
-    {'id': 'CAACAgIAAxkBAAENHjlnNPseKqlpa_H5RIQj6IvuoDMC8AACP1YAAoMXKEmiEycmHVj67jYE', 'chance': 0.30, 'message': 'Вам выпал спокойный Ярик'},
-    {'id': 'CAACAgIAAxkBAAENHjtnNPsr8oIZOvVZ7_HBkuCPh9AoLgACKV4AAmRuIEkZvtwFcE0HhTYE', 'chance': 0.06, 'message': 'Вам выпал крутой Андрей'},
-    {'id': 'CAACAgIAAxkBAAENHj9nNPs-kG0I0GHjgsj7rhSfISsCZQACSVgAArP9IEm1kxkRQuYoyDYE', 'chance': 0.03, 'message': 'вам выпал поцелуй Андрей'},
-    {'id': 'CAACAgIAAxkBAAENHkFnNPtIHYj5qIA5ibGxDaIU84K86AACcl8AAswhIEnLjoI8r-nWkTYE', 'chance': 0.35, 'message': 'Вам выпал подозрительный Руслан'},
-    {'id': 'CAACAgIAAxkBAAENHkNnNPtXR5FYva3yr6HXMlH-knF8RwACIFoAAoWmIEmx3YJp4eH3tDYE', 'chance': 0.10, 'message': 'Вам выпал колдун Егор'},
-    {'id': 'CAACAgIAAxkBAAENHkVnNPthltMOKvUm5S1WtotyUKtS7wACy2MAAs-jIUl6kdUF41G13zYE', 'chance': 0.05, 'message': 'Вам выпал мак комбо делюкс 3 сыра и 1 грибок'},
-    {'id': 'CAACAgIAAxkBAAENHkdnNPttbgRVd6lKt0Tjbgr5qC5migACZFsAAnZVIEkr9v1sYeW78jYE', 'chance': 0.35, 'message': 'Вам выпал Тигран щекастый'},
-    {'id': 'CAACAgIAAxkBAAENHklnNPt5YKseT8E5jYXENrQPWyRfWgAC4lsAAqPzIEkB-jrX-WEbmTYE', 'chance': 0.2, 'message': 'Вам выпал дотер Елисей'},
-    {'id': 'CAACAgIAAxkBAAENHktnNPul3k2TorDo69_-BjQQ5tke5gAC-VoAAgNsIUkrUjlVWIR0ZjYE', 'chance': 0.35, 'message': 'Вам выпал победитель Влад'},
-    {'id': 'CAACAgIAAxkBAAENHk1nNPuu5zn5XPuasYOWBcHrG3qawgACC1cAAjXcKUnKxRuqq1W1tDYE', 'chance': 0.40, 'message': 'Вам выпало 2 крипа'}
+    {
+        'id': 'CAACAgIAAxkBAAENHXtnNGgqh3m9skg-FG6YcPXGdvHHDAACyVcAAlDTIEkyq5UzKXmolDYE',
+        'chance': 0.30,
+        'message': 'Вам выпал монсруозный Ярик',
+        'traits': {'сила': 15}
+    },
+    {
+        'id': 'CAACAgIAAxkBAAENHihnNPqdbLnF0zh0LU5NVooyQ9su9wACvFYAAvS8KEmi8QT8WAQcAAE2BA',
+        'chance': 0.35,
+        'message': 'Вам выпал ужаленеый Тигран',
+        'traits': {'устойчивость': 10}
+    },
+    {
+        'id': 'CAACAgIAAxkBAAENHipnNPquoj1u2zoYgZ4E43IqTY9kowACbWkAAkw1KUmjU1q0J3wLmDYE',
+        'chance': 0.05,
+        'message': 'Вам выпал бешеный Елисей',
+        'traits': {'броня': 25}
+    },
+    {
+        'id': 'CAACAgIAAxkBAAENHi9nNPq86ruNvyBHZhnESc7DBXGI6wAC3lkAAgL1IUkUjqkTjshZtDYE',
+        'chance': 0.35,
+        'message': 'Вам выпал репер Влад',
+        'traits': {'ловкость': 7}
+    },
+    {
+        'id': 'CAACAgIAAxkBAAENHjFnNPr63UkpFsJk4ffA6VsozGj4vAACEVIAAqGBKUkzzUP2Z6TtnzYE',
+        'chance': 0.03,
+        'message': 'Вам выпал гига Руслан',
+        'traits': {"сила": 50, "устойчивость": 50, "броня": 50, "ловкость": 50, "счастье": 50, "спокойствие": 50,"скрытность": 50, "аура": 50, "спиритизм": 50, "фимоз": 50, "уверенность": 50, "криворукость": 50, "хайп": 50}
+    },
+    {
+        'id': 'CAACAgIAAxkBAAENHjNnNPsFZJV8BLLgy2QcO507mceK6QACZFoAAjUoIUn5izprfvq-PzYE',
+        'chance': 0.10,
+        'message': 'Вам выпал округленый Руслан',
+        'traits': {'сила': 15}
+    },
+    {
+        'id': 'CAACAgIAAxkBAAENHjVnNPsUgpSdUu6LxkVuwQIO-BHGegACoVcAApbiKUmvO0oITRkKwzYE',
+        'chance': 0.25,
+        'message': 'Вам выпал счастливый Кирюша',
+        'traits': {'счастье': 15}
+    },
+    {
+        'id': 'CAACAgIAAxkBAAENHjlnNPseKqlpa_H5RIQj6IvuoDMC8AACP1YAAoMXKEmiEycmHVj67jYE',
+        'chance': 0.30,
+        'message': 'Вам выпал спокойный Ярик',
+        'traits': {'спокойствие': 10}
+    },
+    {
+        'id': 'CAACAgIAAxkBAAENHjtnNPsr8oIZOvVZ7_HBkuCPh9AoLgACKV4AAmRuIEkZvtwFcE0HhTYE',
+        'chance': 0.06,
+        'message': 'вам выпал крутой Андрей',
+        'traits': {"сила": 26, "устойчивость": 26, "броня": 26, "ловкость": 26, "счастье": 26, "спокойствие": 26,"скрытность": 26, "аура": 26, "спиритизм": 26, "фимоз": 26, "уверенность": 26, "криворукость": 26, "хайп": 26}
+    },
+    {
+        'id': 'CAACAgIAAxkBAAENHj9nNPs-kG0I0GHjgsj7rhSfISsCZQACSVgAArP9IEm1kxkRQuYoyDYE',
+        'chance': 0.03,
+        'message': 'вам выпал поцелуй Андрей',
+        'traits': {'хайп': 52}
+    },
+    {
+        'id': 'CAACAgIAAxkBAAENHkFnNPtIHYj5qIA5ibGxDaIU84K86AACcl8AAswhIEnLjoI8r-nWkTYE',
+        'chance': 0.35,
+        'message': 'Вам выпал подозрительный Руслан',
+        'traits': {'скрытность': 25}
+    },
+    {
+        'id': 'CAACAgIAAxkBAAENHkNnNPtXR5FYva3yr6HXMlH-knF8RwACIFoAAoWmIEmx3YJp4eH3tDYE',
+        'chance': 0.10,
+        'message': 'Вам выпал колдун Егор',
+        'traits': {'аура': 18}
+    },
+    {
+        'id': 'CAACAgIAAxkBAAENHkVnNPthltMOKvUm5S1WtotyUKtS7wACy2MAAs-jIUl6kdUF41G13zYE',
+        'chance': 0.05,
+        'message': 'Вам выпал мак комбо делюкс 3 сыра и 1 грибок',
+        'traits': {'спиритизм': 15, 'фимоз': -2, 'уверенность': -4, 'криворукость': 15}
+    },
+    {
+        'id': 'CAACAgIAAxkBAAENHkdnNPttbgRVd6lKt0Tjbgr5qC5migACZFsAAnZVIEkr9v1sYeW78jYE',
+        'chance': 0.35,
+        'message': 'Вам выпал Тигран щекастый',
+        'traits': {'счвстья': 12}
+    },
+    {
+        'id': 'CAACAgIAAxkBAAENHklnNPt5YKseT8E5jYXENrQPWyRfWgAC4lsAAqPzIEkB-jrX-WEbmTYE',
+        'chance': 0.2,
+        'message': 'Вам выпал дотер Елисей',
+        'traits': {'фимоз': 14}
+    },
+    {
+        'id': 'CAACAgIAAxkBAAENHktnNPul3k2TorDo69_-BjQQ5tke5gAC-VoAAgNsIUkrUjlVWIR0ZjYE',
+        'chance': 0.35,
+        'message': 'Вам выпал победитель Влад',
+        'traits': {'уверенность': 20}
+    },
+    {
+        'id': 'CAACAgIAAxkBAAENHk1nNPuu5zn5XPuasYOWBcHrG3qawgACC1cAAjXcKUnKxRuqq1W1tDYE',
+        'chance': 0.4,
+        'message': 'Вам выпало 2 крипа',
+        'traits': {'спиритизм': 5, 'броня': 18}
+    },
 ]
